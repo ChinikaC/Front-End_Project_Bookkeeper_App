aa416e565e4cd23a16ed4abb3657f70235b303d1
ChinikaC <clcharles@hotmail.co.uk>
2023-03-22 13:03:53 +0000

    Trying to fix hover element on button

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,17 @@ import React, {useState} from "react";
 // import devitoBook from './images/devitoBook.jpeg';
 
 const Home = () => {
-    const [isHovering, setIsHovering] = useState("");
+    const [isHovering, setIsHovering] = useState(false)
+    const [whichbutton, setWhichButton] = useState(null)
+    const summaries = { 
+        "Dystopian Fiction": "Scary",
+        "Coming-Of-Age Fiction": "Fiction"
+}    
 
-    const handleMouseOver = () => {
+
+    const handleMouseOver = (e) => {
       setIsHovering(true);
+      setWhichButton(e.target.value)
     };
   
     const handleMouseOut = () => {
@@ -32,8 +39,10 @@ const Home = () => {
             </article>
             <h2>Books by Genre</h2>
             <article id="bookByGenre">
-                <button>Dystopian Fiction</button>
-                <button>Coming-Of-Age Fiction</button>
+                <button onMouseOver={handleMouseOver}
+           onMouseOut={handleMouseOut}>Dystopian Fiction</button>
+                <button onMouseOver={handleMouseOver}
+           onMouseOut={handleMouseOut}>Coming-Of-Age Fiction</button>
                 <button>Regency Romance</button>
                 <button>Literary Fiction</button>
                 <button>High Fantasy</button>
@@ -42,11 +51,57 @@ const Home = () => {
                 <button>Political Satire</button>
                 <button>Magical Realism</button>
                 <button>Modernist fiction</button>
-                <button onClick="SkillsButton">Autobiographical fiction</button>
+                <button onMouseOver={handleMouseOver}
+           onMouseOut={handleMouseOut}>Autobiographical fiction</button>
                 <button>Gothic Fiction</button>
             </article>
+         <div id="button1">
+            <button onMouseOver={handleMouseOver}
+           onMouseOut={handleMouseOut}>Dystopian Fiction</button>
+           {isHovering && (
+            <div>
+             <h2>About Dystopian Fiction</h2>
+             <h2>Scary</h2>
+             </div>
+         )}
+       </div>
+            <div id="button2">
+            <button onMouseOver={handleMouseOver}
+           onMouseOut={handleMouseOut}>Coming-Of-Age Fiction</button>
+           {isHovering && (
+            <div>
+             <h2>About fiction</h2>
+             <h2>Scary</h2>
+             </div>
+         )}
+            </div>
+
+            {isHovering ?
+<div>
+<h2>{whichbutton}</h2>
+<p>{summaries[whichbutton]}</p>
+</div> :
+null }
         </section>
     )
 }
 
 export default Home;
+
+// const summaries = { "dystopian fiction": "scary"}
+// const [ishovering, set...] = usestate(false)
+// const [whichbutton ...] = usestate(null)
+
+//handlemousehover = (e) => {
+    //setishovering(true)
+    //setwhichbutton(e.target.value)
+//}
+
+//<button onhover={handlemousehover} onmouseout={handlemouseout}/>dystopian fiction</button>
+
+// {isHovering ?
+// <div>
+// <h2>{whichbutton}</h2>
+// <p>{summaries[whichbutton]}
+// </div> :
+// null }
