14f73a599cb813828204f77445d30aa32ff77b1a
J-emery <jamzwj@gmail,com>
2023-03-22 14:10:23 +0000

    added tippy and filter

--- src/pages/OurBooks.js
+++ src/pages/OurBooks.js
@@ -1,13 +1,52 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import BookList from "../components/BookList"
 
-const OurBooks = ({books, postOwnedBook}) => {
+const OurBooks = ({ books, postOwnedBook }) => {
+
+    const [filteredBooks, setFilteredBooks] = useState([]);
+
+    useEffect(() => {
+        setFilteredBooks(books)}, [books])
+
+    const genres = books.map(book => book.genre)
+
+    const uniqueGenres = genres.filter((genre, index) => { return genres.indexOf(genre) === index })
+
+    const listOfGenres = uniqueGenres.map((genre, index) => {
+        return (
+            <option key={index} value={genre}>
+                {genre}
+            </option>
+        )
+    })
+
+    const filterBooks = (e) => {
+        if (e.target.value === "all") {
+            setFilteredBooks(books);
+        } else {
+            console.log(e.target.value)
+            //filter books and set filtered books to that
+            const filtered = books.filter(book => { return book.genre === e.target.value })
+            setFilteredBooks(filtered)
+        }
+    }
+
     return (
         <>
-        <h2>
-            Our Books
-        </h2>
-        <BookList books={books} postOwnedBook={postOwnedBook} ></BookList>
+            <h2>
+                Our Books
+            </h2>
+            <div>
+                Filter by genre:
+
+                <select
+                    onChange={filterBooks}
+                    name="BookStatus">
+                    <option value="all">All books</option>
+                    {listOfGenres}
+                </select>
+            </div>
+            <BookList books={filteredBooks} postOwnedBook={postOwnedBook} ></BookList>
         </>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,107 +1,53 @@
-import React, {useState} from "react";
+import React, { useState, useRef } from "react";
+import { useOnHoverOutside } from "../hooks/useOnHover";
 // import devitoBook from './images/devitoBook.jpeg';
+import 'react-tippy/dist/tippy.css'
+import { Tooltip } from "react-tippy";
 
 const Home = () => {
-    const [isHovering, setIsHovering] = useState(false)
-    const [whichbutton, setWhichButton] = useState(null)
-    const summaries = { 
-        "Dystopian Fiction": "Scary",
-        "Coming-Of-Age Fiction": "Fiction"
-}    
 
+  return (
+    <section>
+      <h2>Our Book of the Day</h2>
+      <article id="bookOfTheDay">
+        <div>
+          User Review
+        </div>
+        <div>
+          User Rating
+        </div>
+        <div>
+          Book Picture
+          {/* <img src={devitoBook} alt="image of Danny Devito cokouring book" /> */}
+        </div>
+        <div>
+          Blurb
+        </div>
+      </article>
+      <h2>Books by Genre</h2>
+      <article id="bookByGenre">
+        <Tooltip
+          title="Distopian fiction is about something or other, idk"
+          position="bottom-end">
+          <button>Dystopian Fiction
+          </button>
+        </Tooltip>
+        <button >Coming-Of-Age Fiction</button>
+        <button>Regency Romance</button>
+        <button>Literary Fiction</button>
+        <button>High Fantasy</button>
+        <button>Science Fiction</button>
+        <button>Crime Fiction</button>
+        <button>Political Satire</button>
+        <button>Magical Realism</button>
+        <button>Modernist fiction</button>
+        <button >Autobiographical fiction</button>
+        <button>Gothic Fiction</button>
+      </article>
 
-    const handleMouseOver = (e) => {
-      setIsHovering(true);
-      setWhichButton(e.target.value)
-    };
-  
-    const handleMouseOut = () => {
-      setIsHovering(false);
-    };
-
-    return (
-        <section>
-            <h2>Our Book of the Day</h2>
-            <article id="bookOfTheDay">
-                <div>
-                    User Review
-                </div>
-                <div>
-                    User Rating
-                </div>
-                <div>
-                    Book Picture
-                    {/* <img src={devitoBook} alt="image of Danny Devito cokouring book" /> */}
-                </div>
-                <div>
-                    Blurb
-                </div>
-            </article>
-            <h2>Books by Genre</h2>
-            <article id="bookByGenre">
-                <button onMouseOver={handleMouseOver}
-           onMouseOut={handleMouseOut}>Dystopian Fiction</button>
-                <button onMouseOver={handleMouseOver}
-           onMouseOut={handleMouseOut}>Coming-Of-Age Fiction</button>
-                <button>Regency Romance</button>
-                <button>Literary Fiction</button>
-                <button>High Fantasy</button>
-                <button>Science Fiction</button>
-                <button>Crime Fiction</button>
-                <button>Political Satire</button>
-                <button>Magical Realism</button>
-                <button>Modernist fiction</button>
-                <button onMouseOver={handleMouseOver}
-           onMouseOut={handleMouseOut}>Autobiographical fiction</button>
-                <button>Gothic Fiction</button>
-            </article>
-         <div id="button1">
-            <button onMouseOver={handleMouseOver}
-           onMouseOut={handleMouseOut}>Dystopian Fiction</button>
-           {isHovering && (
-            <div>
-             <h2>About Dystopian Fiction</h2>
-             <h2>Scary</h2>
-             </div>
-         )}
-       </div>
-            <div id="button2">
-            <button onMouseOver={handleMouseOver}
-           onMouseOut={handleMouseOut}>Coming-Of-Age Fiction</button>
-           {isHovering && (
-            <div>
-             <h2>About fiction</h2>
-             <h2>Scary</h2>
-             </div>
-         )}
-            </div>
-
-            {isHovering ?
-<div>
-<h2>{whichbutton}</h2>
-<p>{summaries[whichbutton]}</p>
-</div> :
-null }
-        </section>
-    )
+    </section>
+  )
 }
 
 export default Home;
 
-// const summaries = { "dystopian fiction": "scary"}
-// const [ishovering, set...] = usestate(false)
-// const [whichbutton ...] = usestate(null)
-
-//handlemousehover = (e) => {
-    //setishovering(true)
-    //setwhichbutton(e.target.value)
-//}
-
-//<button onhover={handlemousehover} onmouseout={handlemouseout}/>dystopian fiction</button>
-
-// {isHovering ?
-// <div>
-// <h2>{whichbutton}</h2>
-// <p>{summaries[whichbutton]}
-// </div> :
-// null }
