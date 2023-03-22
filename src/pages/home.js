a64a69fe74bc256aebc94aafce21e781ba676691
ryder <ryderzrashid@gmail.com>
2023-03-22 14:07:26 +0000

    home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,30 +1,36 @@
-import React, {useState} from "react";
-// import devitoBook from './images/devitoBook.jpeg';
+import React, { useState } from "react";
+import devitoBook from './images/devitoBook.jpeg';
 
 const Home = () => {
-    const [isHovering, setIsHovering] = useState("");
+    // const [isHovering, setIsHovering] = useState("");
 
-    const handleMouseOver = () => {
-      setIsHovering(true);
-    };
-  
-    const handleMouseOut = () => {
-      setIsHovering(false);
-    };
+    // const handleMouseOver = () => {
+    //     setIsHovering(true);
+    // };
+
+    // const handleMouseOut = () => {
+    //     setIsHovering(false);
+    // };
 
     return (
         <section>
             <h2>Our Book of the Day</h2>
             <article id="bookOfTheDay">
                 <div>
-                    User Review
+                    <ul className="list">
+                        <li className="list-item">An awesome official Danny DeVito-inspired, biographical coloring book</li>
+                        <li className="list-item">Perfect for relaxation and stress relief</li>
+                        <li className="list-item">2 copies of each image, for two chances to color!</li>
+                    </ul>
                 </div>
+
                 <div>
                     User Rating
                 </div>
                 <div>
-                    Book Picture
-                    {/* <img src={devitoBook} alt="image of Danny Devito cokouring book" /> */}
+                    <img src={devitoBook}
+                        alt="image of Danny Devito colouring book"
+                        style={{ width: "50%", margin: "auto" }} />
                 </div>
                 <div>
                     Blurb
@@ -36,25 +42,25 @@ const Home = () => {
                 <button>Classics</button>
                 <button>Non-Fiction</button>
                 <button>Horror</button>
-                <div>
-      <div>
-        <div
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >
-          Hover over me
-        </div>
+                {/* <div>
+                    <div>
+                        <div
+                            onMouseOver={handleMouseOver}
+                            onMouseOut={handleMouseOut}
+                        >
+                            Hover over me
+                        </div>
 
-        {isHovering && (
-          <div>
-            <h2>Horror Genre Info</h2>
-            <h2>Scary</h2>
-          </div>
-        )}
-      </div>
-    </div>
-  );
-                
+                        {isHovering && (
+                            <div>
+                                <h2>Horror Genre Info</h2>
+                                <h2>Scary</h2>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                ); */}
+
                 <button>Adventure</button>
             </article>
         </section>
